4e80ddaa739e1f0be91c49b8d2e75d968552d322
Jack Sullivan <jack@divergent.codes>
2024-10-25 13:42:59 -0700

    Bundle self-signed certs with Golang executable

--- sni-manipulation/server.go
+++ sni-manipulation/server.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"crypto/tls"
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+//go:embed dist/certs/server.crt dist/certs/server.key
+var certsFS embed.FS
+
 func main() {
 	http.HandleFunc("/", handleRequest)
 	fmt.Println("HTTPS Server is running on https://localhost:8443")
-	log.Fatal(http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil))
+
+	certPEMBytes, err := certsFS.ReadFile("dist/certs/server.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	keyPEMBytes, err := certsFS.ReadFile("dist/certs/server.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cert, err := tls.X509KeyPair(certPEMBytes, keyPEMBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := &http.Server{
+		Addr:    ":8443",
+		Handler: nil, // Use the default ServeMux
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		},
+	}
+
+	log.Fatal(server.ListenAndServeTLS("", ""))
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
